4af6c2db0e2b5d9ada12ca8f3426612b6770e6cc
Kimbayeva Tamiris <kimbaevat@gmail.com>
2022-12-19 17:47:12 +0600

    начало

--- index.js
+++ index.js
@@ -3,5 +3,19 @@ import express from 'express'
 const app = express();
 
 app.get('/', (req, res) => {
-    
+    res.send('Hello World!')
 })
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err)
+    }
+
+    console.log('Server OK')
+})
+
+//req - что приходит от клиента на бэк
+//res - бэкенд отправляет клиенту ответ
+
+//чтобы express отправил мы не не пишем экспорт, мы пишем app.listen
+//4444 - нз порта и дальше указываем если будет ошибка 
