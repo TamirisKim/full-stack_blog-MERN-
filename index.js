7151bf53867d4afc56199c0bb373f20559d1697b
Kimbayeva Tamiris <kimbaevat@gmail.com>
2022-12-20 10:47:30 +0600

    понимает json

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from 'express'
 
 const app = express();
+app.use(express.json()) //теперь наше express приложение понимает json
 
 app.get('/', (req, res) => {
-    res.send('Hello World! hhh')
+    res.send('Hello World!')
 })
+
+app.post('/auth/login', (req, res) => {
+    res.json({
+        success: true,
+    })
+})
+
 
 app.listen(4444, (err) => {
     if (err) {
