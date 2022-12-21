2fb283affccddf05ab9e15e83069e3c34ab7f373
Kimbayeva Tamiris <kimbaevat@gmail.com>
2022-12-21 12:36:47 +0600

    зашиф пароль, сох в монго и всю инфу перед в json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
 import {validationResult} from 'express-validator'
 
@@ -17,22 +18,26 @@ app.use(express.json());
 
 
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
  const errors = validationResult(req)
  if(!errors.isEmpty()){
     return res.status(400).json(errors.array())
  }
 
+const password = req.body.password;
+const salt = await bcrypt.genSalt(10);
+const passwordHash = await bcrypt.hash(password, salt)
+
 const doc = new UserModel({
     email: req.body.email,
     fullName: req.body.fullName,
-    passwordHash: req.body.passwordHash,
+    passwordHash,
     avatarUrl: req.body.avatarUrl,
 })
 
- res.json({
-    success: true,
- })
+const user = await doc.save();
+
+ res.json(user)
 })
 
 
