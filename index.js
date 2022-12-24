dc4b048aeb4708d56494c7ff1fc5b27ae6038ee2
Kimbayeva Tamiris <kimbaevat@gmail.com>
2022-12-24 14:39:43 +0600

    можем найти себя

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ import mongoose from 'mongoose'
 import {validationResult} from 'express-validator'
 
 import { registerValidation } from './validations/auth.js';
+
 import UserModel from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
+
 
 mongoose
     .connect('mongodb+srv://admin:RRRRRR@cluster0.6fxr3ja.mongodb.net/blog?retryWrites=true&w=majority')
@@ -99,8 +102,26 @@ try{
 }
 })
 
-app.get('/auth/me', (req, res) => {
-    
+app.get('/auth/me', checkAuth, async (req, res) => {
+    try{
+        const user = await UserModel.findById(req.userId)
+
+        if (!user){
+            return res.status(404).json({
+                message: 'Пользователь не найден', 
+            })
+        }
+
+        const {passwordHash, ...userData} = user._doc;
+
+        res.json(userData);
+        
+    } catch (err){
+        console.log(err)
+        res.status(500).json({
+            message: 'Не доступа',
+        })
+    }
 })
 
 app.listen(4444, (err) => {
