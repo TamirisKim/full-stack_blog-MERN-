a80acda10a3ec1f825c134ebd39301740566aed5
Kimbayeva Tamiris <kimbaevat@gmail.com>
2022-12-26 10:58:20 +0600

    удл, созд, обн, получ все, получ 1 статью

--- index.js
+++ index.js
@@ -24,17 +24,11 @@ app.post('/auth/register', registerValidation, UserController.register)
 app.get('/auth/me', checkAuth, UserController.getMe)
 
 
-// app.get('/posts', PostController.getAll);
-// app.get('/posts/:id', PostController.getOne);
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
-// app.delete('/posts/:id', checkAuth, PostController.remove);
-// app.patch(
-//     '/posts/:id',
-//     checkAuth,
-//     postCreateValidation,
-//     handleValidationErrors,
-//     PostController.update,
-// );
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, PostController.update);
 
 app.listen(4444, (err) => {
     if (err) {
